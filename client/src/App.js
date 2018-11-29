1467769315aa6d01a67c542e4a1439b116751c10
Justin Hastings <justin.hastings@vanderbilt.edu>
2018-11-29 11:03:39 -0600

    Downvote

--- client/src/App.js
+++ client/src/App.js
@@ -113,6 +113,9 @@ class App extends Component {
             <button>
               upvote
             </button>
+            <button>
+              downvote
+            </button>
           </li>
         </Router>);
 
